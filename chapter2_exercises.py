b52895624f277a6273322515a30f548c20229385
eudaimonious <eudaimonious@gmail.com>
2012-12-18 12:38:27 -0500

    30 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,39 @@
 # Exercises for chapter 2: Problems 2.1, 2.2, 2.3, and 2.4 in Think Python
 
-# Name
+# Margaret Scott
+'''
+Exercise 2.1
+The interpreter is evaluating the integers as octal numbers due to the leading zero.
+'''
+
+'''
+Exercise 2.2
+The script has no output without a print statement.
+'''
+print 5
+x=5
+print x
+print x+1
+
+'''
+Exercise 2.3
+1. 8
+2. 8.5
+3. 4.0
+4. 11
+5. '.....'
+'''
+
+#Exercise 2.4
+print (4.0/3.0) * 3.1415 * (5**3)
+print "Answer to 1 is 523.58333..."
+print (24.95 * .6) *60 + (3 + .75 *59)
+print "Answer to 2 is 945.44999..."
+start_in_seconds = (6*60+52)*60
+easy_pace_in_seconds = 8 * 60 + 15
+tempo_in_seconds =  7 * 60 + 12
+finish_in_seconds = start_in_seconds + 2 * easy_pace_in_seconds + 3 * tempo_in_seconds
+finish_hour = finish_in_seconds / 60**2
+finish_minutes = (finish_in_seconds % 60 **2) / 60
+print finish_hour,":",finish_minutes
+print "Answer to 3 is 7:30"
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,80 @@
 # Exercises for chapter 3: Problems 3.1, 3.2, 3.3, and 3.4 in Think Python
 
-# Name
+# Margaret Scott
+
+'''
+Exercise 3.1
+
+repeat_lyrics()
+def print_lyrics():
+    print "I'm a lumberjack, and I'm okay."
+    print "I sleep all night and I work all day."
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+NameError: name 'repeat_lyrics' is not defined
+'''
+
+# Exercise 3.2
+
+def print_lyrics():
+    print "I'm a lumberjack, and I'm okay."
+    print "I sleep all night and I work all day."
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+repeat_lyrics()
+
+'''
+The programs prints print_lyrics (the two strings) twice, for a total of four strings:
+I'm a lumberjack, and I'm okay.
+I sleep all night and I work all day.
+I'm a lumberjack, and I'm okay.
+I sleep all night and I work all day.
+
+'''
+
+#Exercise 3.3
+
+def right_justify(s):
+    print " "*(70-len(s)) + s
+
+right_justify('allen')
+
+#Exercise 3.4
+
+'''
+#Part 1
+def do_twice(f):
+    f()
+    f()
+    
+def print_spam():
+    print 'spam'
+    
+do_twice(print_spam)
+'''
+#Part 2
+def do_twice(f, v):
+    f(v)
+    f(v)
+    
+#Part 3
+def print_twice(x):
+    print x
+    print x
+'''    
+#Part 4
+do_twice(print_twice, 'spam')
+'''
+#Part 5
+def do_four(a, b):
+    do_twice(a, b)
+    do_twice(a, b)
+
+do_four(print_twice, 'Hello world!')
+
